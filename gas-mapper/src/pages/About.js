d55ad0ee380963dec3725c114eb71dd29c32b9af
alexgruden <agruden@uvm.edu>
2025-09-08 12:46:44 -0400

    Added Essential info to about page, created navbutton

--- gas-mapper/src/pages/About.js
+++ gas-mapper/src/pages/About.js
@@ -1,11 +1,47 @@
 export default function About() {
     return (
-        <div className="flex justify-center">
-            <h1 className="text-2xl font-bold mt-10">About Page</h1>
-            <h3></h3>
-            <p>
-                
-            </p>
+        <div className="flex flex-col items-center justify-center mt-10">
+            <div className="w-3/5 justify-center">
+                <h1 className="text-3xl font-bold justify-center">About Page</h1>
+                <h2 className="text-xl justify-center font-bold"><br/>YieldEstate</h2>
+                <h2 className="text-xl justify-center font-bold"><br/>Project Description</h2>
+                <p>This project is created to be similar to the app "Zillow". Its a simple website but holds powerful features. 
+                My goal is to expand on what is possible for myself in terms of mixing business/housing analytics into fullstack development.<br/><br/>
+                The site will contain:
+                </p>
+                <ul className="list-disc list-inside">
+                    <li>An analyzer which takes in data and immediately renders results upon submission.</li>
+                    <li>Interactive Property search within a map</li>
+                    <li>An ML Model which attempts to predict property prices</li>
+                    <li>An integrated summary assitant with additional helping capabilities </li>
+                    <li>Ability to be deployed for live demonstrations</li>
+                </ul>
+                <h2 className="text-xl justify-center font-bold"><br/>Technologies Used</h2>
+                <p>
+                    Project Programming languages:
+                </p>
+                <ul className="list-disc list-inside">
+                    <li>JavaScript</li>
+                    <li>CSS</li>
+                </ul>
+                <p>
+                    Project Libraries
+                </p>
+                <ul className="list-disc list-inside">
+                    <li>React</li>
+                    <li>React Router</li>
+                </ul>
+                <p>
+                    Project Frameworks
+                </p>
+                <ul className="list-disc list-inside">
+                    <li>Tailwind CSS</li>
+                </ul>
+                <h2 className="text-xl justify-center font-bold"><br/>Installation Instructions</h2>
+                <h2 className="text-xl justify-center font-bold"><br/>Usage Instructions</h2>
+                <h2 className="text-xl justify-center font-bold"><br/>Project Status</h2>
+                <h2 className="text-xl justify-center font-bold"><br/>Project Walk Through</h2>
+            </div>
         </div>
     );
   }
--- gas-mapper/src/pages/Home.js
+++ gas-mapper/src/pages/Home.js
@@ -1,6 +1,22 @@
+import { Link } from "react-router-dom"
+
 export default function Home() {
     return (
-        <div><h1 className="text-2xl font-bold">Home Page</h1></div>
-        
+        <div className="flex flex-col items-center justify-center mt-10">
+            <div className="w-3/5 justify-center">
+                <h1 className="text-2xl font-bold">Home Page</h1>
+                <Button />
+                <Link to="/evaluate">Evaluate</Link>
+                <Link to="/history">History</Link>
+                <Link to="/about">About</Link>
+            </div>
+        </div>
+        );
+  }
+
+  function Button() {
+    return (
+        <Link t>
+        </Link>
     );
   }
